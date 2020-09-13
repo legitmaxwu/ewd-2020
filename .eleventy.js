320bb9c2e05ae969f7980bdabbf6e60bbbb89833
Kevin Tang <kevintang2023@gmail.com>
2020-09-12 21:54:57 -0400

    Favicon

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("js");
+
+    
+    eleventyConfig.addPassthroughCopy("favicon.ico");
+    eleventyConfig.addPassthroughCopy("android-chrome-192x192.png");
+    eleventyConfig.addPassthroughCopy("android-chrome-512x512.png");
+    eleventyConfig.addPassthroughCopy("site.webmanifest");
+    
+
+    return {
+        passthroughFileCopy: true,
+        dir: {
+            input: "views",
+            output: "_site",
+        },
+        htmlTemplateEngine: "liquid",
+        markdownTemplateEngine: "liquid",
+    }
+};
